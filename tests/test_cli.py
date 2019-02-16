ec86883d9f8443882c5f7067125d6e09d726b2ab
Albert Tugushev <albert@tugushev.ru>
2019-02-16 04:55:34 +0300

    test with encoded linesep

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -449,7 +449,7 @@ def test_default_index_url():
 
     assert status == 0
     expected = (
-        '  -i, --index-url TEXT            Change index URL (defaults to\r\n'
+        '  -i, --index-url TEXT            Change index URL (defaults to' + os.linesep.encode('utf-8')
         #'                                  http://example.com)'
     )
     assert expected in output
