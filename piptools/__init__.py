6fe4f9b2793ed79405147568f4b3da42da4a9323
Albert Tugushev <albert@tugushev.ru>
2019-07-30 16:30:10 +0300

    Remove obsolete pip's dimensions from CI matrix

--- piptools/__init__.py
+++ piptools/__init__.py
@@ -1,6 +1,14 @@
 import locale
+import os
+import sys
 
 from piptools.click import secho
+
+# Inject vendored directory into system path.
+vendored_path = os.path.abspath(
+    os.path.sep.join([os.path.dirname(os.path.realpath(__file__)), "_vendored"])
+)
+sys.path.insert(0, vendored_path)
 
 # Needed for locale.getpreferredencoding(False) to work
 # in pip._internal.utils.encoding.auto_decode
